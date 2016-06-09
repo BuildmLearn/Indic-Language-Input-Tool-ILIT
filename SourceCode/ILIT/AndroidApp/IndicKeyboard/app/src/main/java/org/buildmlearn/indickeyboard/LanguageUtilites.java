bae2cfbdfdaa0dc9b8f62a85ef1079f442b29e12
bhavita <bhavitalalwani@gmail.com>
2016-06-09 09:17:46 +0530

    Improved Language Utilities,Added Extended Adaptive area support

--- SourceCode/ILIT/AndroidApp/IndicKeyboard/app/src/main/java/org/buildmlearn/indickeyboard/LanguageUtilites.java
+++ SourceCode/ILIT/AndroidApp/IndicKeyboard/app/src/main/java/org/buildmlearn/indickeyboard/LanguageUtilites.java
@@ -3,16 +3,12 @@ package org.buildmlearn.indickeyboard;
 public class LanguageUtilites {
     private static final int hindi_firstConsonant = 2325;
     private static final int hindi_firstExtendedConsonant = 2358;
-    private static int[] hindi_dependentVowels= new int[]{2381, 2366, 2367, 2368, 2369, 2370, Constants.adaptive_vowel_to_consonantCombinations, 2375, 2376, 2379, 2380, 2306, 2307, Constants.extended_adaptive};
-    private static int[] hindi_dependentVowels_land=new int[]{2381,2366,2367,2368,2369,2370,2375,2376, 2379, 2380, 2306, 2307,Constants.adaptive_vowel_to_consonantCombinations,Constants.extended_adaptive};
-    private static int[] hindi_independentVowels = new int[]{2309, 2310, 2311, 2312, 2313, 2314, Constants.adaptive_consonantCombinations_to_vowel, 2319, 2320, 2323, 2324, 2306, 2307, Constants.extended_adaptive};
-    private static int[] hindi_independentVowels_land=new int[]{2309, 2310, 2311, 2312, 2313, 2314, 2319, 2320, 2323, 2324, 2306, 2307, Constants.adaptive_consonantCombinations_to_vowel,Constants.extended_adaptive};
+    private static int[] hindi_dependentVowels= new int[]{2381, 2366, 2367, 2368, 2369, 2370,2375, 2376, 2379, 2380, 2306, 2307,2305,2364,2373,2374,2377,2378,2402,2403 };
+    private static int[] hindi_independentVowels = new int[]{2309, 2310, 2311, 2312, 2313, 2314,2319, 2320, 2323, 2324, 2306, 2307,2305,2308, 2365, 2315,2316,2318,2321,2322,2355,2356,2400,2401,2384 };
     private static final int gujarati_firstConsonant = 2709;
     private static final int gujarati_firstExtendedConsonant = 2742;
-    private static int[] gujarati_dependentVowels= new int[]{2765,2750,2751,2752,2753,2754, Constants.adaptive_vowel_to_consonantCombinations, 2759 ,2760, 2763, 2764, 2690, 2691, Constants.extended_adaptive};
-    private static int[] gujarati_dependentVowels_land=new int[]{2765,2750,2751,2752,2753, 2754, 2759 ,2760, 2763, 2764, 2690, 2691,Constants.adaptive_vowel_to_consonantCombinations,Constants.extended_adaptive};
-    private static int[] gujarati_independentVowels = new int[]{2693, 2694, 2695, 2696,2697, 2698, Constants.adaptive_consonantCombinations_to_vowel, 2703, 2704, 2707,2708, 2690, 2691, Constants.extended_adaptive};
-    private static int[] gujarati_independentVowels_land=new int[]{2693, 2694, 2695, 2696,2697, 2698, 2703, 2704, 2707,2708, 2690, 2691, Constants.adaptive_consonantCombinations_to_vowel,Constants.extended_adaptive};
+    private static int[] gujarati_dependentVowels= new int[]{2765,2750,2751,2752,2753,2754, 2759 ,2760, 2763, 2764, 2690, 2691,	2689, 2748, 2755,2756,	2757,	2761,2786,2786 };
+    private static int[] gujarati_independentVowels = new int[]{2693, 2694, 2695, 2696,2697, 2698,2703, 2704, 2707,2708, 2690, 2691, 2749,2699,2700,2784,2785,2801,2768 };
     private static int[] error = new int[]{-1};
 
 
@@ -43,35 +39,81 @@ public class LanguageUtilites {
 
     public static int[] getDependentVowels(String language,String displayMode) {
         if (language == "hindi") {
-            int[] temp =   (displayMode=="")?hindi_dependentVowels:hindi_dependentVowels_land;
-            return temp;
+            return getAdaptiveArea(hindi_dependentVowels,Constants.adaptive_vowel_to_consonantCombinations,0,displayMode);
         }
 
         else if (language == "gujarati") {
-            int[] temp =   (displayMode=="")?gujarati_dependentVowels:gujarati_dependentVowels_land;
-            return temp;
+            return getAdaptiveArea(gujarati_dependentVowels,Constants.adaptive_vowel_to_consonantCombinations,0,displayMode);
         }
-
 
         return error;
     }
+
+    public static int[] getDependentVowelsExtended(String language,String displayMode) {
+        if (language == "hindi") {
+            return getAdaptiveArea(hindi_dependentVowels,Constants.adaptive_vowel_to_consonantCombinations,12,displayMode);
+        }
+
+        else if (language == "gujarati") {
+            return getAdaptiveArea(gujarati_dependentVowels,Constants.adaptive_vowel_to_consonantCombinations,12,displayMode);
+        }
+
+        return error;
+    }
+
+    public static int[] getAdaptiveArea(int[] t,int mode,int startPos,String displayMode)
+    {
+        int temp[]=new int[14];//fixed always
+        temp[12]=mode;
+        temp[13]=Constants.extended_adaptive;
+        int high=(startPos==0)?13:t.length-startPos;
+        for(int i=0,j=startPos;i<high; i++)
+        {
+            if(displayMode=="" && i==6)
+            {
+                temp[i]=mode;
+                temp[12]=0; //clearing it
+            }
+            else {
+                temp[i] = t[j++];
+            }
+
+        }
+
+
+        return temp;
+    }
+
 
     public static int[] getIndependentVowels(String language,String displayMode) {
 
         if (language == "hindi") {
-            return (displayMode=="")?hindi_independentVowels:hindi_independentVowels_land;
+            return getAdaptiveArea(hindi_independentVowels,Constants.adaptive_consonantCombinations_to_vowel,0,displayMode);
         }
 
 
         else   if (language == "gujarati") {
-            return (displayMode=="")?gujarati_independentVowels:gujarati_independentVowels_land;
+            return getAdaptiveArea(gujarati_independentVowels,Constants.adaptive_consonantCombinations_to_vowel,0,displayMode);
         }
 
         return error;
 
     }
 
+    public static int[] getIndependentVowelsExtended(String language,String displayMode) {
 
+        if (language == "hindi") {
+            return getAdaptiveArea(hindi_independentVowels,Constants.adaptive_consonantCombinations_to_vowel,12,displayMode);
+        }
+
+
+        else   if (language == "gujarati") {
+            return getAdaptiveArea(gujarati_independentVowels,Constants.adaptive_consonantCombinations_to_vowel,12,displayMode);
+        }
+
+        return error;
+
+    }
 
 
 
@@ -100,10 +142,11 @@ public class LanguageUtilites {
     public static int IndependentPretext(int code,String language)
     {
         //This is usually required, when the letter is Independent Vowel, but requires pretext say
+
         int temp=-1;
         if(language=="hindi")
         {
-            if(code == 2306 || code == 2307)
+            if(code==2305 || code==2306 ||code==2307)
             {
                 temp= hindi_independentVowels[0] ;
 
@@ -121,6 +164,18 @@ public class LanguageUtilites {
     }
 
     /*
+    Just a simple utility function to see if part of array
+     */
+    public static boolean contains(int[] array, final int key) {
+        for (int i : array) {
+            if (i == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /*
     Checks if the key is dependent Vowel or not
 
      */
@@ -129,18 +184,19 @@ public class LanguageUtilites {
     {
         if(language=="hindi") {
             //As per Devanagari IndicSyllabic UCD Documentation
-            return (code == 2306) || (code == 2307) || (code == 2362) || code == 2363 || (code >= 2366 && code <= 2383) || (code >= 2389 && code <= 2391) || (code == 2402) || (code == 2403);
+            return contains(hindi_dependentVowels,code);
         }
 
         else if(language=="gujarati")
         {
-            return   (code>=2689 && code <=2691 ) || (code>=2750 && code <=2765 ) || (code>=2750 && code <=2765 ) || code==2786 ||  code==2787;
+            return contains(gujarati_dependentVowels,code);
         }
 
-        return false;
+        return false; //language not found
 
     }
 
 
 }
+
 
