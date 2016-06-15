28b561a87c9e61bf5b902a61e9fda149c690cb5c
bhavita <bhavitalalwani@gmail.com>
2016-06-15 09:25:37 +0530

    Changed some constants

--- SourceCode/ILIT/AndroidApp/IndicKeyboard/app/src/main/java/org/buildmlearn/indickeyboard/Constants.java
+++ SourceCode/ILIT/AndroidApp/IndicKeyboard/app/src/main/java/org/buildmlearn/indickeyboard/Constants.java
@@ -3,26 +3,25 @@ package org.buildmlearn.indickeyboard;
 public class Constants {
 
     public static final int SPACE_KEY = 32;
-    public static final int DELIMITER_KEY =124 ;
-    public static final int main_adaptive=-103;
-    public static final int adaptive_dependent=-102;
-    public static final int adaptive_consonantFillsCombinations=-101;
-    public static final int adaptive_consonantCombinations_to_vowel=-100;
-    public static final int adaptive_vowel_to_consonantCombinations=-99;
-    public static final int extended_adaptive =-98;
+    public static final int DELIMITER_KEY = 124;
+    public static final int KEYCODE_OPTIONS = -106;
+    public static final int KEYCODE_LANGUAGE_SWITCH = -105;
+    public static final int orientation_retain_adaptive = -104;
+    public static final int main_adaptive = -103;
+    public static final int adaptive_dependent = -102;
+    public static final int adaptive_consonantFillsCombinations = -101;
+    public static final int adaptive_consonantCombinations_to_vowel = -100;
+    public static final int adaptive_vowel_to_consonantCombinations = -99;
+    public static final int extended_adaptive = -98;
     public static final int SYMBOL = -97;
-    public static final int PHONEPAD =-89 ;
+    public static final int ABC=-90;
+    public static final int PHONEPAD = -89;
     public static final int main_to_extended_consonant = -94;
-    public static final int extended_consonant_to_main=-93;
-    public static final int CurrentKeyboard_MAIN=0;
-    public static final int CurrentKeyboard_EXTENDED=1;
-    public static final int CurrentKeyboard_PHONEPAD=2;
-    public static final int CurrentKeyboard_SYMBOL=3;
-    public static final int KEYCODE_OPTIONS = -100;
-    public static final int KEYCODE_LANGUAGE_SWITCH = -101;
-
-
-
+    public static final int extended_consonant_to_main = -93;
+    public static final int CurrentKeyboard_MAIN = 0;
+    public static final int CurrentKeyboard_EXTENDED = 1;
+    public static final int CurrentKeyboard_PHONEPAD = 2;
+    public static final int CurrentKeyboard_SYMBOL = 3;
 
 }
 
